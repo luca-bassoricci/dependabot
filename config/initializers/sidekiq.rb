810e16660cb0eb4cf9f913fc5ebdd29663bda3cf
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2020-06-28 09:18:23 +0300

    Use one log formatter

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,6 +3,6 @@
 require_relative "../log_formatter"
 
 Sidekiq.configure_server do |config|
-  config.log_formatter = SidekiqLogFormatter.new
+  config.log_formatter = SimpleLogFormatter.new
   config.logger.datetime_format = DATETIME_FORMAT
 end
