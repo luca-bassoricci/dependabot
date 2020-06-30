da60b550bb2dd82d0aa329d7ec322236b06f7981
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2020-06-30 13:48:09 +0300

    Add api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ require "sidekiq/cron/web"
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount Sidekiq::Web => "/sidekiq"
+
+  namespace :api, defaults: { format: :json } do
+    resources :dependabot, only: [:create]
+  end
 end
