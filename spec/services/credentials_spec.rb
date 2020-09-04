a2a80daaa59ebda95218455146dd5bcf3df91473
Robin Weinreich <robin.weinreich@posteo.de>
2020-09-04 09:02:41 +0000

    Feat: Maven repository credentials

--- spec/services/credentials_spec.rb
+++ spec/services/credentials_spec.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 describe Credentials do
+  let(:github_token) { nil }
+  let(:maven_repo_url) { nil }
+  let(:maven_repo_user) { nil }
+  let(:maven_repo_pass) { nil }
+
   let(:gitlab_creds) do
     {
       "type" => "git_source",
@@ -19,17 +24,31 @@ describe Credentials do
     }
   end
 
+  let(:maven_repo_creds) do
+    {
+      "type" => "maven_repository",
+      "url" => Settings.credentials_maven_repository_url,
+      "username" => Settings.credentials_maven_repository_username,
+      "password" => Settings.credentials_maven_repository_password
+    }
+  end
+
   subject { described_class.fetch }
 
   before do
     described_class.instance_variable_set(:@credentials, nil)
-    Settings.add_source!({ github_access_token: github_token })
+    Settings.add_source!(
+      {
+        github_access_token: github_token,
+        credentials_maven_repository_url: maven_repo_url,
+        credentials_maven_repository_username: maven_repo_user,
+        credentials_maven_repository_password: maven_repo_pass
+      }
+    )
     Settings.reload!
   end
 
   context "Credentials returns" do
-    let(:github_token) { nil }
-
     it "only gitlab creds" do
       expect(subject).to eq([gitlab_creds])
     end
@@ -40,6 +59,16 @@ describe Credentials do
 
     it "gitlab and githubs creds" do
       expect(subject).to eq([github_creds, gitlab_creds])
+    end
+  end
+
+  context "Credentials returns" do
+    let(:maven_repo_url) { "url" }
+    let(:maven_repo_user) { "username" }
+    let(:maven_repo_pass) { "password" }
+
+    it "gitlab and maven creds" do
+      expect(subject).to eq([gitlab_creds, maven_repo_creds])
     end
   end
 end
