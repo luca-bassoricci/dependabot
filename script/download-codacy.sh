ccb408b3ce2a1b63b230ef93963ec7502e6cae2c
Andrejs <andrejs.cunskis@gmail.com>
2020-09-06 21:04:57 +0000

    dev: Add codacy analysis

---
+++ script/download-codacy.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+set -e
+
+EXECUTABLE="codacy-coverage-reporter-${CODACY_VERSION}"
+
+[ -f "$EXECUTABLE" ] && exit
+
+apt-get update -q && apt-get install -y -q --no-install-recommends curl
+curl -Ls -o "$EXECUTABLE" "https://dl.bintray.com/codacy/Binaries/${CODACY_VERSION}/codacy-coverage-reporter-linux"
+chmod +x "$EXECUTABLE"
