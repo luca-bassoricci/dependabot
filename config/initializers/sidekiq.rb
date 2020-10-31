1331e473d0dfd4ac3e99f6cfae0f91e6feeeddc2
Andrejs <andrejs.cunskis@gmail.com>
2020-10-31 15:31:54 +0000

    Add redis password

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,4 +5,9 @@ require_relative "../log_formatter"
 Sidekiq.configure_server do |config|
   config.log_formatter = SimpleLogFormatter.new
   config.logger.datetime_format = DATETIME_FORMAT
+  config.redis = { password: ENV["REDIS_PASSWORD"] } if ENV["REDIS_PASSWORD"]
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { password: ENV["REDIS_PASSWORD"] } if ENV["REDIS_PASSWORD"]
 end
