a207fc077349338b482fb632f0296477f69bbd88
Andrejs <andrejs.cunskis@gmail.com>
2020-11-07 12:12:24 +0000

    Use only local cache for private runners

--- script/build.sh
+++ script/build.sh
@@ -2,18 +2,21 @@
 
 # Build script for image building on CI
 
-cmd=$1
-path=$2
-image=$3
-addr=$([ "$cmd" == "buildctl" ] && echo "--addr tcp://buildkit-service.gitlab.svc.cluster.local:1234" || echo "")
+path=$1
+image=$2
+args="--frontend=dockerfile.v0 \
+--local context=$path \
+--local dockerfile=$path \
+--opt build-arg:COMMIT_SHA=$CI_COMMIT_SHA \
+--opt build-arg:PROJECT_URL=$CI_PROJECT_URL \
+--output type=image,\"name=$image:$CURRENT_TAG,$image:$LATEST_TAG\",push=true"
 
-$cmd $addr build \
-  --frontend=dockerfile.v0 \
-  --local context="$path" \
-  --local dockerfile="$path" \
-  --opt build-arg:COMMIT_SHA="$CI_COMMIT_SHA" \
-  --opt build-arg:PROJECT_URL="$CI_PROJECT_URL" \
-  --export-cache type=inline \
-  --import-cache type=registry,ref="$image":master-latest \
-  --import-cache type=registry,ref="$image:$LATEST_TAG" \
-  --output type=image,\"name="$image:$CURRENT_TAG","$image:$LATEST_TAG"\",push=true
+function build() {
+  buildctl --addr tcp://buildkit-service.gitlab.svc.cluster.local:1234 build $args
+}
+
+function build_fork() {
+  buildctl-daemonless.sh build $args --export-cache type=inline --import-cache type=registry,ref="$image:$LATEST_TAG"
+}
+
+[[ -z "$FORK" ]] || build_fork && build
