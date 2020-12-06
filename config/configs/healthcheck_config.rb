4b9f495ef2d18bb01d1a5ddc2455daf142a09ac5
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2020-12-06 11:37:32 +0200

    dev: use global tmp folder for healthcheck

--- config/configs/healthcheck_config.rb
+++ config/configs/healthcheck_config.rb
@@ -4,5 +4,5 @@ class HealthcheckConfig < ApplicationConfig
   env_prefix :settings_
 
   attr_config queue: "healthcheck-#{ENV['HOSTNAME']}",
-              filename: "tmp/healthcheck.txt"
+              filename: "/tmp/healthcheck.txt"
 end
