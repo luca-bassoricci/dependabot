0c5987de597e790966cd2fc10d3020005d492472
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2020-12-10 12:01:36 +0200

    dev: configure client and server options for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 
-redis_config = {
-  password: ENV["REDIS_PASSWORD"],
-  timeout: 1,
-  reconnect_attempts: 3
-}
 Redis.exists_returns_integer = true
 
-Sidekiq.configure_server do |config|
-  config.logger = DependabotLogger.logger
-  config.redis = redis_config
-  config.options[:queues].push("default", HealthcheckConfig.queue)
+Sidekiq.tap do |sidekiq|
+  sidekiq.logger = DependabotLogger.logger
+  sidekiq.options[:queues].push(HealthcheckConfig.queue)
+  sidekiq.redis = {
+    password: ENV["REDIS_PASSWORD"],
+    timeout: 1,
+    reconnect_attempts: 3
+  }
 end
-Sidekiq.configure_client { |config| config.redis = redis_config }
 
 # Reduce verbose output of activejob
 ActiveJob::Base.logger = Logger.new(IO::NULL)
