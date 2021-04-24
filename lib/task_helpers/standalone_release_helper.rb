554b3e6719782cf3c86fef0f1910707842a5fa0b
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2021-04-24 20:08:26 +0300

    chore: fix regex for current version detection in dependabot-standalone

--- lib/task_helpers/standalone_release_helper.rb
+++ lib/task_helpers/standalone_release_helper.rb
@@ -42,7 +42,7 @@ class StandaloneReleaseHelper
   #
   # @return [String]
   def current_version
-    @current_version ||= gitlab_ci.match(/#{IMAGE}:(?<tag>\w+)/)[:tag]
+    @current_version ||= gitlab_ci.match(/name: #{IMAGE}:(?<tag>[0-9.]+)/)[:tag]
   end
 
   # Updated .gitlab-ci.yml file
