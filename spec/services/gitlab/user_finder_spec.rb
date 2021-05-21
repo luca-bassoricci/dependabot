f9ec98d368efe066de0abfecac7113dbdd4ecd5d
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2021-05-21 21:38:30 +0300

    fix: Additionally filter gitlab search results

--- spec/services/gitlab/user_finder_spec.rb
+++ spec/services/gitlab/user_finder_spec.rb
@@ -3,18 +3,30 @@
 describe Gitlab::UserFinder, epic: :services, feature: :gitlab do
   subject(:user_finder_return) { described_class.call(usernames) }
 
-  let(:gitlab) { instance_double("Gitlab::Client", user_search: [user]) }
-  let(:user) { OpenStruct.new(id: 1) }
-  let(:usernames) { %w[test test2] }
+  let(:gitlab) { instance_double("Gitlab::Client", user_search: [user1, user2]) }
+  let(:user1) { OpenStruct.new(id: 1, username: "test") }
+  let(:user2) { OpenStruct.new(id: 2, username: "test2") }
 
   before do
     allow(Gitlab::Client).to receive(:new) { gitlab }
   end
 
-  it "returns array with ids" do
-    expect(user_finder_return).to eq([1, 1])
-    aggregate_failures do
-      usernames.each { |username| expect(gitlab).to have_received(:user_search).with(username) }
+  context "with existing user" do
+    let(:usernames) { %w[test] }
+
+    it "returns array with ids" do
+      aggregate_failures do
+        expect(user_finder_return).to eq([1])
+        expect(gitlab).to have_received(:user_search).with("test")
+      end
+    end
+  end
+
+  context "with non existing user" do
+    let(:usernames) { %w[non-existing] }
+
+    it "returns array with ids" do
+      expect(user_finder_return).to be_nil
     end
   end
 end
