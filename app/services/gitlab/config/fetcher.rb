8db4f95b8c39b07a166d351f1b77810790a5ec59
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2021-07-09 14:13:26 +0300

    improvement: option to override branch of dependabot.yml configuration file

--- app/services/gitlab/config/fetcher.rb
+++ app/services/gitlab/config/fetcher.rb
@@ -6,27 +6,27 @@ module Gitlab
 
     class Fetcher < ApplicationService
       # @param [String] project_name
-      def initialize(project_name, default_branch)
+      def initialize(project_name, branch)
         @project_name = project_name
-        @default_branch = default_branch
+        @branch = branch
       end
 
       # Get dependabot.yml file contents
       #
       # @return [String]
       def call
-        log(:info, "Fetching configuration for #{project_name} from #{default_branch}")
-        gitlab.file_contents(project_name, AppConfig.config_filename, default_branch)
+        log(:info, "Fetching configuration for #{project_name} from #{branch}")
+        gitlab.file_contents(project_name, AppConfig.config_filename, branch)
       rescue Error::NotFound
         raise(
           MissingConfigurationError,
-          "#{AppConfig.config_filename} not present in #{project_name}'s branch #{default_branch}"
+          "#{AppConfig.config_filename} not present in #{project_name}'s branch #{branch}"
         )
       end
 
       private
 
-      attr_reader :project_name, :default_branch
+      attr_reader :project_name, :branch
     end
   end
 end
---
+++ spec/services/dependabot/config_fetcher_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+describe Dependabot::ConfigFetcher, epic: :services, feature: :configuration do
+  include_context "with dependabot helper"
+
+  let(:project) { "project" }
+  let(:default_branch) { "main" }
+
+  before do
+    allow(Gitlab::DefaultBranch).to receive(:call).with(kind_of(String)) { default_branch }
+    allow(Gitlab::Config::Fetcher).to receive(:call) { raw_config }
+  end
+
+  context "without custom branch configuration" do
+    it "fetches config from default branch" do
+      expect(described_class.call(project)).to eq(dependabot_config)
+      expect(Gitlab::Config::Fetcher).to have_received(:call).with(project, default_branch)
+    end
+
+    it "fetches single entry of config from default branch" do
+      expect(described_class.call(project, find_by: { package_manager: "bundler" })).to eq(
+        dependabot_config.find { |entry| entry[:package_manager] == "bundler" }
+      )
+    end
+  end
+
+  context "with custom branch configuration" do
+    let(:branch) { "custom_branch" }
+
+    before do
+      allow(AppConfig).to receive(:config_branch) { branch }
+    end
+
+    it "fetches config from configured branch" do
+      expect(described_class.call(project)).to eq(dependabot_config)
+      expect(Gitlab::Config::Fetcher).to have_received(:call).with(project, branch)
+    end
+  end
+end
