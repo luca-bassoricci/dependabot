7b3095c0747b7629a0a18c62f49a7cee4814e39c
andrejs <andrejs.cunskis@gmail.com>
2021-07-12 17:49:25 +0000

    chore: simplify ci ruby image

--- script/download-codacy.sh
+++ script/download-codacy.sh
@@ -8,6 +8,5 @@ EXECUTABLE="codacy-coverage-reporter-${CODACY_VERSION}"
 
 [ -f "$EXECUTABLE" ] && exit
 
-apt-get update -q && apt-get install -y -q --no-install-recommends curl
 curl -Ls -o "$EXECUTABLE" "https://dl.bintray.com/codacy/Binaries/${CODACY_VERSION}/codacy-coverage-reporter-linux"
 chmod +x "$EXECUTABLE"
