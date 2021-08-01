1ec911de50dd4b62250676fe0f5c1fd56e4fa635
andrejs <andrejs.cunskis@gmail.com>
2021-08-01 11:07:40 +0000

    chore: sentry configuration improvements

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,13 +1,31 @@
 # frozen_string_literal: true
 
-return if ENV["SIDEKIQ_HEALTHCHECK"]
-
 require "sentry-ruby"
 require "sentry-rails"
 require "sentry-sidekiq"
 
 Sentry.init do |config|
   config.enabled_environments = ["production"]
-  config.traces_sample_rate = 0.5
   config.release = "dependabot-gitlab@#{ENV['VERSION']}"
+  config.skip_rake_integration = true
+  config.logger = DependabotLogger.logger
+  config.traces_sampler = lambda do |sampling_context|
+    transaction_context = sampling_context[:transaction_context]
+    op = transaction_context[:op]
+    transaction_name = transaction_context[:name]
+
+    case op
+    when /request/
+      case transaction_name
+      when /healthcheck/
+        0.0
+      else
+        1.0
+      end
+    when /sidekiq/
+      1.0
+    else
+      0.0
+    end
+  end
 end
