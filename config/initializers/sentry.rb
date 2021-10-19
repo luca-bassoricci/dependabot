364e82a65664eec4b8ec3c5da2e7048e971a687a
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2021-10-19 21:04:03 +0300

    chore: fix sentry release

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
-return if ENV["SIDEKIQ_HEALTHCHECK"]
-
 require "sentry-ruby"
 require "sentry-rails"
 require "sentry-sidekiq"
 
 Sentry.init do |config|
   config.enabled_environments = ["production"]
-  config.release = "dependabot-gitlab@#{ENV['VERSION']}"
+  config.release = "dependabot-gitlab@#{ENV['APP_VERSION']}"
   config.skip_rake_integration = true
   config.logger = DependabotLogger.logger
   config.traces_sample_rate = AppConfig.sentry_traces_sample_rate
