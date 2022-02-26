ffa8d90d7813a153e53d9bfa0cd8f9245b1a1b5b
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2022-02-26 23:14:30 +0100

    Add spec for configuration fetching
    changelog: chore

--- spec/models/config_spec.rb
+++ spec/models/config_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-describe Config, epic: :models do
+describe Config, :integration, epic: :models do
   subject(:config) { described_class.new(dependabot_config) }
 
   include_context "with dependabot helper"
@@ -9,17 +9,31 @@ describe Config, epic: :models do
   let(:expected_registries) { dependabot_config.first[:registries].first }
   let(:actual_registries) { config.mongoize.first[:registries].first }
 
-  it "encrypts registries credentials for storage" do
-    pass = EncryptHelper.decrypt(actual_registries["password"])
+  context "with credentials" do
+    it "encrypts registries credentials for storage" do
+      pass = EncryptHelper.decrypt(actual_registries["password"])
 
-    expect(pass).to eq(password)
+      expect(pass).to eq(password)
+    end
+
+    it "decrypts credentials for use" do
+      expect(described_class.demongoize(config.mongoize)).to eq(config)
+    end
+
+    it "sanitizes auth fields in registries" do
+      expect(config.sanitize.first[:registries]).to eq([expected_registries.except("password")])
+    end
   end
 
-  it "decrypts credentials for use" do
-    expect(described_class.demongoize(config.mongoize)).to eq(config)
-  end
+  context "with persisted credentials" do
+    let(:project) { Project.new(name: repo, config: dependabot_config) }
 
-  it "sanitizes auth fields in registries" do
-    expect(config.sanitize.first[:registries]).to eq([expected_registries.except("password")])
+    before do
+      project.save!
+    end
+
+    it "correctly fetches config" do
+      expect(Project.find_by(name: repo).config.entry(package_ecosystem: "bundler")).to eq(dependabot_config.first)
+    end
   end
 end
