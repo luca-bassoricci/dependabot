6288dfe781d4c7f5bd1658eccdd9f7c24af0a2e5
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2022-07-05 07:30:11 +0000

    Deprecate branch name configurations via environment variables
    changelog: chore

--- config/configs/dependabot_config.rb
+++ config/configs/dependabot_config.rb
@@ -8,4 +8,26 @@ class DependabotConfig < ApplicationConfig
               branch_name_separator: "-",
               open_pull_request_limit: 5,
               config_filename: ".gitlab/dependabot.yml"
+
+  def branch_name_separator
+    if ENV["SETTINGS__BRANCH_NAME_SEPARATOR"]
+      ApplicationHelper.log(
+        :warn,
+        "Setting branch name separator via environment variable is deprecated, please use global configuration file!"
+      )
+    end
+
+    super
+  end
+
+  def open_pull_request_limit
+    if ENV["SETTINGS__OPEN_PULL_REQUEST_LIMIT"]
+      ApplicationHelper.log(
+        :warn,
+        "Setting open pull request limit via environment variable is deprecated, please use global configuration file!"
+      )
+    end
+
+    super
+  end
 end
