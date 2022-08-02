a8f1127d8db82bb234e4f22051739f3ee865e3b7
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2022-08-02 12:03:35 +0300

    Use correct gitlab access token for project sync in UI
    changelog: fix

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -4,7 +4,7 @@ class ProjectController < ApplicationController
   def update
     project = Project.find_by(id: params.require(:id))
 
-    Dependabot::Projects::Creator.call(project.name)
+    Dependabot::Projects::Creator.call(project.name, project.gitlab_access_token)
     Cron::JobSync.call(project)
 
     redirect_to root_path
