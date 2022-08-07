b4e36fdecf8ddc786860c40173c0498b852db023
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2022-08-07 14:29:22 +0300

    Add custom ruby warning processing
    changelog: improvement

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,9 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+
+# Custom warning processing
+require "warning"
+
+Warning.ignore(/Pattern matching is experimental/)
+Warning.process { |warning| ApplicationHelper.log(:warn, warning) }
