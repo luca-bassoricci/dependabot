f936c2ade081b7504cf6f3705bc0f309f86af70f
andrejs <andrejs.cunskis@gmail.com>
2022-09-25 19:54:03 +0000

    Add more information to log context
    changelog: chore

--- app/models/update_job.rb
+++ app/models/update_job.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
+# Dependency update job
+#
+# @!attribute package_ecosystem
+#   @return [String]
+# @!attribute directory
+#   @return [String]
+# @!attribute cron
+#   @return [String]
+# @!attribute last_executed
+#   @return [DateTime]
+# @!attribute run_errors
+#   @return [Array]
+# @!attribute project
+#   @return [Project]
+# @!attribute log_entries
+#   @return [Array<LogEntry>]
+#
 class UpdateJob
   include Mongoid::Document
 
@@ -8,7 +25,15 @@ class UpdateJob
   field :cron, type: String
   field :last_executed, type: DateTime
   field :run_errors, type: Array, default: []
-  field :run_log, type: Array, default: []
 
   belongs_to :project
+  embeds_many :log_entries
+
+  # Persist log entries
+  #
+  # @param [Array<Hash>] logs
+  # @return [void]
+  def save_log_entries(logs)
+    logs.map { |entry| LogEntry.new(**entry, update_job: self) }.each(&:save!)
+  end
 end
