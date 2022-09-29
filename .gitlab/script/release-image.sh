beafca15b52f296f76a9281f11e898309a5cd5d3
andrejs <andrejs.cunskis@gmail.com>
2022-09-29 10:59:08 +0000

    Fix release jobs and image publishing
    changelog: ci

--- .gitlab/script/release-image.sh
+++ .gitlab/script/release-image.sh
@@ -9,9 +9,9 @@ source "$(dirname "$0")/utils.sh"
 release_version="$(echo $CI_COMMIT_TAG | grep -oP 'v\K[0-9.]+')"
 
 log_info "Tagging and pushing release to dockerhub"
-docker buildx imagetools create -t "${APP_IMAGE}" "${DOCKERHUB_IMAGE}:${release_version}"
-docker buildx imagetools create -t "${APP_IMAGE}" "${DOCKERHUB_IMAGE}:latest"
+docker buildx imagetools create -t "${DOCKERHUB_IMAGE}:${release_version}" "${APP_IMAGE}"
+docker buildx imagetools create -t "${DOCKERHUB_IMAGE}:latest" "${APP_IMAGE}"
 
 log_info "Tagging and pushing release to gitlab registry"
-docker buildx imagetools create -t "${APP_IMAGE}" "${GITLAB_IMAGE}:${release_version}"
-docker buildx imagetools create -t "${APP_IMAGE}" "${GITLAB_IMAGE}:latest"
+docker buildx imagetools create -t "${GITLAB_IMAGE}:${release_version}" "${APP_IMAGE}"
+docker buildx imagetools create -t "${GITLAB_IMAGE}:latest" "${APP_IMAGE}"
