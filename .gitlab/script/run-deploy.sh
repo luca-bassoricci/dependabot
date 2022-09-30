1f1354ab7b45c5e48ef5a757e4196e1d7752119a
andrejs <andrejs.cunskis@gmail.com>
2022-09-30 09:47:41 +0000

    Make sure migration runs successfully in deploy test job
    changelog: ci

--- .gitlab/script/run-deploy.sh
+++ .gitlab/script/run-deploy.sh
@@ -5,11 +5,14 @@ set -euo pipefail
 source "$(dirname "$0")/utils.sh"
 
 log_with_header "Setting up dependabot app"
-log "** Pulling image '${APP_IMAGE}' **"
-docker pull --quiet $APP_IMAGE
+log "** Pulling docker images **"
+docker compose pull --quiet --include-deps
+
+log "** Running migrations **"
+docker compose run --rm migration
 
 log "** Starting app **"
-docker compose --ansi always up -d --quiet-pull
+docker compose up --wait redis mongodb web worker
 
 log_with_header "Setting up gitlab mock"
 log "** Pulling image '${MOCK_IMAGE}' **"
@@ -25,6 +28,3 @@ docker run -d \
 
 log "** Setting mock expectations **"
 script/set-mock.sh deploy docker
-
-log "** Waiting for dependabot to be ready **"
-timeout 30 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' docker:3000/healthcheck)" != "200" ]]; do sleep 2; done'
