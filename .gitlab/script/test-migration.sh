e1f89c5aef900f32ea69c5ebac7b9b3c8a8559b8
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2022-10-02 10:56:39 +0300

    Update seeded test data
    changelog: chore

--- .gitlab/script/test-migration.sh
+++ .gitlab/script/test-migration.sh
@@ -12,12 +12,13 @@ docker pull --quiet "$release_image"
 
 log "** Initializing database from latest release **"
 APP_IMAGE="$release_image" docker compose run --rm migration 2>/dev/null
-
-log "** Running seed data **"
-docker compose run --rm migration bundle exec rake "db:seed" 2>/dev/null
+APP_IMAGE="$release_image" docker compose run --rm migration bundle exec rake "db:seed" 2>/dev/null
 
 log "** Running migrations **"
 docker compose run --rm migration 2>/dev/null
 
 log "** Validate no migrations are pending **"
 docker compose run --rm migration bundle exec rake "dependabot:check_migrations" 2>/dev/null
+
+log "** Running seed data **"
+docker compose run --rm migration bundle exec rake "db:seed" 2>/dev/null
